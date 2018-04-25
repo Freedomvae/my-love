28b5ec0688f0919ee7fe79d873d776f409da60f7
lenovo <1105101603@qqcom>
2018-04-25 18:04:35 +0800

    modify

---
+++ 002consle/alert.js
@@ -0,0 +1,12 @@
+<script>
+function TaoLu() {
+    alert("你的过去我来不及参与")
+    alert("你的未来我奉陪到底")
+}
+alert("未曾青梅，青梅枯萎，芬芳满地")
+TaoLu();
+alert("不见竹马，竹马老去，相思万里")
+TaoLu();
+alert("我明白你会来")
+alert("所以我等");
+</script>
